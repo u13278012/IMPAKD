d3db80a286a2f3c62bacced1d50561366a3e33b6
Diana <diana.kawuma@gmail.com>
2016-07-14 11:17:14 +0200

    added import section for bootstrap

--- Implementation/Front End/PIOAPP/ember-cli-build.js
+++ Implementation/Front End/PIOAPP/ember-cli-build.js
@@ -24,6 +24,11 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  
+  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff', {
+  destDir: 'fonts'
+});	
+
    
   return app.toTree();
 };
