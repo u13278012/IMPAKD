a2ea9aaad094122491c11649227a511a03b73955
Diana <diana.kawuma@gmail.com>
2016-07-15 12:25:15 +0200

    added handlebars to the homePage

--- Implementation/Front End/PIOAPP/app/routes/home-page.js
+++ Implementation/Front End/PIOAPP/app/routes/home-page.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(){
+		let homeArray = [
+			{
+				name: "3 Bedroom House",
+				price: "R 1 995 000",
+				ROI: "15%"
+			
+			},{
+				name: "1.5 Bedroom Apartment",
+				price: "R 330 000",
+				ROI: "5%"
+			},{
+				name: "3 Bedroom Townhouse",
+				price: "R 1 250 000",
+				ROI: "20%"
+			}			
+		];
+		return homeArray;
+	}
 });
