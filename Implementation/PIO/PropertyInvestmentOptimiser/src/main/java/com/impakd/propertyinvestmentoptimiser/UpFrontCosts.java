8cb654e4655ff43d95d49b7c5908120fec44e3ce
Kudzai Muranga <murangakudzai@gmail.com>
2016-07-22 11:29:43 +0200

    New Project added

---
+++ Implementation/PIO/PropertyInvestmentOptimiser/src/main/java/com/impakd/propertyinvestmentoptimiser/UpFrontCosts.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.impakd.propertyinvestmentoptimiser;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Khumalo
+ */
+@Entity
+@XmlRootElement
+public class UpFrontCosts implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    private double conveyancingFees ;
+    
+    @OneToOne
+    private double vatDebit ;
+    
+    @OneToOne
+    private double deedsFees ;
+    
+    @OneToOne
+    private double initiationFee;
+
+    public double getInitiationFee() {
+        return initiationFee;
+    }
+
+    public void setInitiationFee(double initiationFee) {
+        this.initiationFee = initiationFee;
+    }
+
+    public double getDeedsFees() {
+        return deedsFees;
+    }
+
+    public void setDeedsFees(double deedsFees) {
+        this.deedsFees = deedsFees;
+    }
+    
+    public double getVatDebit() {
+        return vatDebit;
+    }
+
+    public void setVatDebit(double vatDebit) {
+        this.vatDebit = vatDebit;
+    }
+
+    public double getConveyancingFees() {
+        return conveyancingFees;
+    }
+
+    public void setConveyancingFees(double conveyancingFees) {
+        this.conveyancingFees = conveyancingFees;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UpFrontCosts)) {
+            return false;
+        }
+        UpFrontCosts other = (UpFrontCosts) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ejb.upFrontCosts[ id=" + id + " ]";
+    }
+    
+}
---
+++ Implementation/PIO/PropertyInvestmentOptimiser/src/main/java/service/ApplicationConfig.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Kudzai Muranga
+ */
+@javax.ws.rs.ApplicationPath("rs")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.impakd.propertyinvestmentoptimiser.NewCrossOriginResourceSharingFilter.class);
+        resources.add(service.AdditionalBondRepaymentFacadeREST.class);
+        resources.add(service.AuthenticationFacadeREST.class);
+        resources.add(service.BondFacadeREST.class);
+        resources.add(service.ExpensesFacadeREST.class);
+        resources.add(service.InceasesFacadeREST.class);
+        resources.add(service.PersonFacadeREST.class);
+        resources.add(service.ProfileFacadeREST.class);
+        resources.add(service.PropertyFacadeREST.class);
+        resources.add(service.RentalFacadeREST.class);
+        resources.add(service.ReportFacadeREST.class);
+        resources.add(service.ReservesFacadeREST.class);
+        resources.add(service.StatisticsFacadeREST.class);
+        resources.add(service.UpFrontCostsFacadeREST.class);
+    }
+    
+}
