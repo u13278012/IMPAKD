8b439251baa566ea18f3e494849b68c69e2bfb64
Diana <diana.kawuma@gmail.com>
2016-07-23 16:42:40 +0200

    added register and login page

---
+++ Implementation/PIO/FrontEnd/app/login/login.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {
+    templateUrl: 'login/login.html',
+    controller: 'LoginCtrl'
+  });
+}])
+
+.controller('LoginCtrl', [function() {
+
+}]);
+
+
