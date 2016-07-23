39cc98137009b2a45e049e42f8220bc227bb9a1f
Priscilla Madigoe <u13049128@tuks.co.za>
2016-07-23 14:41:53 +0200

    Moved Files & Added Logo to Index Page

--- Implementation/PIO/FrontEnd/app/app.js
+++ Implementation/PIO/FrontEnd/app/app.js
@@ -1,13 +1,8 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.report',
-  'myApp.home',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+angular.module('myApp', ['ngRoute'])
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
 //  $locationProvider.hashPrefix('!');
 //
 //  $routeProvider.otherwise({redirectTo: '/report'});
