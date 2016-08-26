3435e8ef3dd99eeff48fc01851fab9dc9cca1892
Kudzai Muranga <murangakudzai@gmail.com>
2016-08-26 10:07:57 +0200

    updates

--- Implementation/PIO/FrontEnd/app/login/login.js
+++ Implementation/PIO/FrontEnd/app/login/login.js
@@ -1,26 +1,50 @@
 'use strict';
 
-var app = angular.module('myApp', ['ngRoute']);
+angular.module('myApp', ['ngRoute'])
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', {
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
     templateUrl: 'login/login.html',
     controller: 'LoginCtrl'
   });
+}])
+
+.controller('LoginCtrl', ["$scope", "$window", "$http", function($scope, $window, $http){
+
+$scope.validation = function()
+{
+    if($scope.loginUserName === "" || $scope.loginPassword === "")
+    {
+        alert("Please complete the entire form");
+    }
+    else
+    {
+        $scope.submitFunction();
+    }
+};
+
+$scope.submitFunction = function() 
+    {
+            var encodedString = 'UserName=' +
+            encodeURIComponent($scope.UserName) +
+            '&Password='+
+            encodeURIComponent($scope.Password);
+       
+
+            $http({
+                method: 'POST',
+                url: 'http://localhost:8080/BackEnd/rs/profile/login/'+$scope.loginUserName/+$scope.loginPassword,
+               // data: encodedString,
+                //headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function(response) {
+                    window.location = "../home/home.html"; 
+
+                }).
+                error(function(response)
+                {
+                    $window.alert(response.status);
+                });
+
+        };
+
 }]);
-
-
-
-app.controller('login',['$Scope', '$http','X2JS', function($Scope, $http, X2JS) {
-    $Scope.username = document.getElementById('UserName').value;
-    $Scope.password = document.getElementById('Password').value;
-    
-    $http.get("http://localhost:8080/BackEnd/rs/profile/login/"+$Scope.username+"/"+$Scope.password)
-    .success(function(data) {
-        var x2js = new X2JS(); 
-        var json = x2js.xml_str2json( data );
-        $Scope.newProfile = json;
-    });
-}]);
-
-
