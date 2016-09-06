e8832681fcce8e125db2b3295df1a2e51fbe2d79
Diana <diana.kawuma@gmail.com>
2016-09-06 17:17:18 +0200

    created class to do accounting calculations

---
+++ Implementation/PIO/BackEnd/src/java/Accounting/accountingExpenses.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Accounting;
+import Entities.Expenses;
+import Entities.Increases;
+import Entities.Bond;
+/**
+ *
+ * @author Diana
+ */
+public class accountingExpenses {
+    
+}
