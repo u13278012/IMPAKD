328f43d1ef32e59f8c37f3f5ce1e8b5476e5a5ad
u12031748 <sandk115@gmail.com>
2016-09-09 10:20:33 +0200

    unit test

--- Implementation/PIO/BackEnd/test/Test/PropertyTest.java
+++ Implementation/PIO/BackEnd/test/Test/PropertyTest.java
@@ -27,7 +27,7 @@ public void initialiseproperty(){
 @Test (expected = NullPointerException.class)
 public void deletePropertyUnitTest() throws Exception{
     Property p;
-    p = property.find((long)2);
+    p = property.find((long)1);
     
   //s  assertEquals("check if it returns the right property", null,p);
 
