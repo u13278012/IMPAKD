efdc0c72e3048b40ffc8427e94fb1daeb69d64e2
u12031748 <sandk115@gmail.com>
2016-10-23 14:50:15 +0200

    report

--- Implementation/PIO/FrontEnd/app/report/report.js
+++ Implementation/PIO/FrontEnd/app/report/report.js
@@ -22,13 +22,42 @@ angular.module('myApp', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/report', {
     templateUrl: 'report/report.html',
-    controller: 'UpdatePropertyCtrl'
+    controller: 'ReportCtrl'
   });
 }])
 
 .controller('ReportCtrl', ["$scope", "$window", "$http", function($scope, $window, $http) {
 
-           
+   $scope.loadpdf = function() 
+       {
+            var session  = localStorage.getItem("session");
+           // var propertyid  = el.id;
+            var encodedString = 'profileID=' +
+            encodeURIComponent(session);
+            
+ 
+          
+          //20285
+               $http({
+                method: 'GET',
+                url: 'http://localhost:8080/BackEnd/rs/tr/pdf',
+                data:  $scope.encodedString,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function(response) {
+                  /* alert("good");
+                    var file = new Blob([response]);
+                    var fileURL = URL.createObjectURL(file);
+                    $scope.content = response;*/
+                }).
+                error(function(response)
+                {
+                  
+                     alert(response);
+                                          
+
+                });
+            
+       }
 
 
 }]);
